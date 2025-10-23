b98f523d51723c9eda8cb8d05b14e47958f7884e
christopher550 <blstchris25@gmail.com>
2025-10-23 15:37:10 +0300

    adds src/data/user.js

--- src/data/user.js
+++ src/data/user.js
@@ -1,11 +1,11 @@
 const user = {
   name: "Liza",
   city: "New York",
-  bio: "I made this!",
+  bio: "I made this",
   color: "firebrick",
   links: {
     github: "https://github.com/liza",
-    linkedin: "https://www.linkedin.com/in/liza/",
+    linkedin: "https://linkedin.com/in/liza",
   },
 };
 
